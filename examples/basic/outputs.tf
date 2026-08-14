ee9f22df72a2ba87a491de5ff3acf5b7e07942cc
Shogo Takaki <en19926@gmail.com>
2026-08-15 00:33:31 +0900

    Delete examples/basic/outputs.tf

--- examples/basic/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "discord_interactions_endpoint_url" {
-  description = "The Lambda Function URL to be set in Discord Developer Portal (Interactions Endpoint URL)"
-  value       = module.discord_gemini.interactions_endpoint_url
-}
-
-output "dynamodb_table_name" {
-  description = "DynamoDB Table Name"
-  value       = module.discord_gemini.dynamodb_table_name
-}
